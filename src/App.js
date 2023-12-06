86547d26765623692e59683228dbe4ace77633e6
gdwood1 <gdwood48@gmail.com>
2023-12-06 11:24:16 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const CardViewer = () => {
+  const [deckId, setDeckId] = useState('');
+  const [cards, setCards] = useState([]);
+  const [remaining, setRemaining] = useState(0);
+  const [isShuffling, setIsShuffling] = useState(false);
+
+  useEffect(() => {
+    async function fetchNewDeck() {
+      try {
+        const response = await fetch('https://deckofcardsapi.com/api/deck/new/');
+        const data = await response.json();
+        setDeckId(data.deck_id);
+        setRemaining(data.remaining);
+      } catch (error) {
+        console.error('Error fetching new deck:', error);
+      }
+    }
+
+    fetchNewDeck();
+  }, []);
+
+  const drawCard = async () => {
+    if (remaining === 0) {
+      alert('Error: no cards remaining!');
+      return;
+    }
+
+    try {
+      const response = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/`);
+      const data = await response.json();
+
+      setCards([...cards, data.cards[0]]);
+      setRemaining(data.remaining);
+    } catch (error) {
+      console.error('Error drawing card:', error);
+    }
+  };
+
+  const shuffleDeck = async () => {
+    if (isShuffling) {
+      return;
+    }
+
+    setIsShuffling(true);
+    setCards([]);
+    try {
+      const response = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/shuffle/`);
+      const data = await response.json();
+
+      setRemaining(data.remaining);
+    } catch (error) {
+      console.error('Error shuffling deck:', error);
+    } finally {
+      setIsShuffling(false);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={drawCard} disabled={remaining === 0 || isShuffling}>
+        Draw a Card
+      </button>
+      <button onClick={shuffleDeck} disabled={isShuffling}>
+        Shuffle Deck
+      </button>
+      <div>
+        {cards.map((card, index) => (
+          <img key={index} src={card.image} alt={card.code} style={{position: 'absolute', top: '10%', left: '10%', transform: `rotate(${Math.ceil(Math.random() * 45) * (Math.round(Math.random()) ? 1 : -1)}deg)`}}/>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default CardViewer;
